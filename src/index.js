14431824b78d8ec9c0d852d925c1a3adb1c4261b
yuyunfrancis <francisberi@yahoo.com>
2021-10-06 12:23:06 +0100

    new repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css"
+const books = [
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    {
+        id: 1,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+        title: 'I love you to the Moon and Back',
+        author: 'Amella Hepworth'
+    },
+
+    {
+        id: 2,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/81snd8WgSHS._AC_UL200_SR200,200_.jpg',
+        title: 'I to the Moon and Back',
+        author: 'Amella Hepworth'
+    },
+    {
+        id: 3,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/91WEOhz%2B9IL._AC_UL200_SR200,200_.jpg',
+        title: 'The christmas pig',
+        author: 'J.K Rowling'
+    }
+]
+
+
+function BookList() {
+    return <section className='booklist'>
+        {books.map((book) => {
+            return (
+                <Book key={book.id} {...book} />
+            )
+        })}
+    </section>
+
+}
+
+const Book = ({ img, title, author }) => {
+
+    const clickHandler = () => {
+        alert('hello world')
+
+    }
+
+    return <article className='book'>
+        <img src={img} alt="" />
+        <h1 onClick={() => console.log({ title })}>{title}</h1>
+        <h4>{author}</h4>
+        <button type="button" onClick={clickHandler}>reference </button>
+        e
+
+    </article>
+}
+
+
+
+
+ReactDom.render(<BookList />, document.getElementById('root'))
